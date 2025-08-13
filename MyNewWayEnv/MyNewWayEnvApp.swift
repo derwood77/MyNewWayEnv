cbfd1e31e71f2b9209296825db3ccf09ade47efe
Derwood Freen <freenderwood@gmail.com>
2025-08-13 20:55:33 +0100

    Initial Commit

---
+++ MyNewWayEnv/MyNewWayEnvApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewWayEnvApp.swift
+//  MyNewWayEnv
+//
+//  Created by me developer on 13/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyNewWayEnvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
